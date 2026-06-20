e92353f55aca354c4f1250c6b4903b3aad8566ba
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2026-06-20 10:15:12 +0800

    we're moving!!🌸

--- assets/js/nav/index.js
+++
@@ -1,9 +0,0 @@
-function showHeaderLogoWhenSize() {
-  if (window.innerWidth < 480) { document.querySelector('img[sumiyoiLogo]').src = '/sumiyoi/assets/images/favicon.png'; }
-  else { document.querySelector('img[sumiyoiLogo]').src = '/sumiyoi/assets/images/logo/dark.png'; }
-}
-
-function headerResize() {
-  if (window.innerWidth < 480) { document.querySelector('img[sumiyoiLogo]').src = '/sumiyoi/assets/images/favicon.png'; }
-  else { document.querySelector('img[sumiyoiLogo]').src = '/sumiyoi/assets/images/logo/dark.png'; }
-}
